bdc2cfce1c6ce17e5603eb636a9e79aa6542b9d1
JuliaDinda21 <145783448+JuliaDinda21@users.noreply.github.com>
2024-03-26 10:47:25 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+function App() {
+  return (
+    <div className="text-white bg-[#141414] min-h-screen grid place-items-center">
+      <div className="w-[80%] sm:w-full max-w-[400px] rounded-xl bg-[#1F1F1F] p-10 flex flex-col items-center gap-4">
+        <img className="rounded-full w-24 aspect-square" src="/Juli.webp" />
+        <div className="text-center">
+          <h1 className="text-2xl font-bold">Julia Dinda Lestari</h1>
+          <p className="text-[#d172ac] font-medium">Jakarta, Indonesia</p>
+        </div>
+        <p className="text-gray-300 text-center text-sm my-[8px]">
+          "Front-end Developer"
+        </p>
+        <div className="flex flex-col gap-4 w-full">
+          <a
+            href="https://www.instagram.com/about_julls/"
+            className="w-full text-white bg-[#333] block text-center py-2 hover:scale-105 active:scale-90 duration-300 rounded-lg font-medium"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.linkedin.com/in/julia-dinda-591930285/"
+            className="w-full text-white bg-[#333] block text-center py-2 hover:scale-105 active:scale-90 duration-300 rounded-lg font-medium"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://github.com/JuliaDinda21"
+            className="w-full text-white bg-[#333] block text-center py-2 hover:scale-105 active:scale-90 duration-300 rounded-lg font-medium"
+          >
+            Github
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
